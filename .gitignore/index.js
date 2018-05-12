eb5bf45662440f651f77a9ff8145926fbe76a1c0
Drakoonney <39219563+Drakoonney@users.noreply.github.com>
2018-05-12 17:26:45 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: *help");
+    console.log("Connected")
+});
+
+bot.login("NDQ0ODE4Njk3NjUyMDExMDEw.DdhdNg.prRVGQZJXXS-o-hSIriWDukYaBE");
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: /n -*help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Salut toi! x)");
+        console.log("Commande Salut effectuée");
+    }
+});
